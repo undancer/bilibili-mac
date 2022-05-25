d66a7be4861de96a7d7abc7a4d58fa8817372123
undancer <>
2022-05-26 03:17:36 +0800

    初始化一个子项目

---
+++ packages/core/src/main.ts
@@ -0,0 +1,4 @@
+const cli = (): string => {
+    return "core"
+}
+export default cli
--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-let app = require('../packages/rollup-starter-lib-ts')
+let app = require('../packages/core')
 console.log("app,", app())
